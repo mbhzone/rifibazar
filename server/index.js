ed834a8785200fe77b9391e8533ab68adffa5061
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-11-11 09:20:23 +0600

    cors access

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5174', // Local frontend
   'https://rifibazar.com/', // Live frontend
+  'https://rifibazar-7vuv.vercel.app/', // Live frontend
 ];
 
 // ✅ Proper CORS Setup (only once)

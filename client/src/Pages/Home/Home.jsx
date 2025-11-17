56fbefe79780d47315fcf5ac7881e83a99a1ccdd
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-11-17 17:43:23 +0600

    comment out code

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -34,8 +34,8 @@ const Home = () => {
           আমাদের পণ্যসমূহ
         </h2>
       </div>
-      {/*
-            <section className="w-full">
+
+      <section className="w-full">
         <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-6 mb-4 px-2 ">
           {Array.isArray(products) &&
             products.map(product => (
@@ -43,7 +43,6 @@ const Home = () => {
             ))}
         </div>
       </section>
-      */}
 
       <ContactIcon></ContactIcon>
       <Footer></Footer>
